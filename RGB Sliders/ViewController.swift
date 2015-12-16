facdc1584fa918ad97692f50f657606f2bcfbd90
Domenic Fiorenza <domenicfiorenza@domenics-macbook-air.local>
2015-12-16 18:20:00 -0500

    Initial Commit

---
+++ RGB Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB Sliders
+//
+//  Created by Domenic Fiorenza on 2015-12-16.
+//  Copyright © 2015 Domenic Fiorenza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
